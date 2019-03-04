948a555d4e7fbc7882e0cbb5839224a430f8490c
Riley Testut <riley@rileytestut.com>
2019-03-04 15:28:53 -0800

    Initial Commit

---
+++ Harmony-Dropbox/Harmony_Dropbox.h
@@ -0,0 +1,19 @@
+//
+//  Harmony_Dropbox.h
+//  Harmony-Dropbox
+//
+//  Created by Riley Testut on 3/4/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Harmony_Dropbox.
+FOUNDATION_EXPORT double Harmony_DropboxVersionNumber;
+
+//! Project version string for Harmony_Dropbox.
+FOUNDATION_EXPORT const unsigned char Harmony_DropboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Harmony_Dropbox/PublicHeader.h>
+
+
